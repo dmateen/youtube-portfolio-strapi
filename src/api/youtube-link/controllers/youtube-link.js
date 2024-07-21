c51a8ab6e3703ffa522eb2f34611f473e75bdad0
dmateen <daniyalmateen22@gmail.com>
2024-07-21 19:33:24 +0500

    Added Youtube API & Removed Pagination

--- src/api/youtube-link/controllers/youtube-link.js
+++ src/api/youtube-link/controllers/youtube-link.js
@@ -1,9 +1,34 @@
-'use strict';
+"use strict";
 
 /**
  * youtube-link controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::youtube-link.youtube-link');
+module.exports = createCoreController(
+  "api::youtube-link.youtube-link",
+  ({ strapi }) => ({
+    async find(ctx) {
+      ctx.query = {
+        ...ctx.query,
+        pagination: {
+          limit: -1,
+        },
+      };
+      // Fetch all youtube-link entries
+      const { data, meta } = await super.find(ctx);
+
+      // Extract links from the response
+      const links = data.map((entry) => entry.attributes.link);
+
+      // Call the service to process the links and get video details
+      const videoDetails = await strapi
+        .service("api::youtube-link.youtube-link")
+        .processLinks(links);
+
+      // Append the video details to the response
+      return { videoDetails, meta };
+    },
+  })
+);
--- src/api/youtube-link/services/youtube-link.js
+++ src/api/youtube-link/services/youtube-link.js
@@ -1,9 +1,69 @@
-'use strict';
+"use strict";
 
 /**
  * youtube-link service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
+const { google } = require("googleapis");
 
-module.exports = createCoreService('api::youtube-link.youtube-link');
+const API_KEY = process.env.YOUTUBE_DEV;
+
+const youtube = google.youtube({
+  version: "v3",
+  auth: API_KEY,
+});
+
+async function getVideoDetails(videoId) {
+  try {
+    const response = await youtube.videos.list({
+      part: "snippet,statistics",
+      id: videoId,
+    });
+    const video = response.data.items[0];
+    if (video) {
+      const title = video.snippet.title;
+      const channel = video.snippet.channelTitle;
+      const views = video.statistics.viewCount;
+      return { title, channel, views };
+    }
+  } catch (error) {
+    console.error(`Error fetching video details: ${error}`);
+  }
+  return null;
+}
+
+module.exports = createCoreService(
+  "api::youtube-link.youtube-link",
+  ({ strapi }) => ({
+    async processLinks(youtubePortfolioLinks) {
+      try {
+          const videoData = [];
+
+        for (const link of youtubePortfolioLinks) {
+          let videoId;
+          if (link.includes("youtu.be")) {
+            videoId = link.split("youtu.be/")[1].split("?")[0];
+          } else if (link.includes("youtube.com")) {
+            videoId = link.split("v=")[1].split("&")[0];
+          }
+          const details = await getVideoDetails(videoId);
+          if (details) {
+            videoData.push({
+              title: details.title,
+              channel: details.channel,
+              views: details.views,
+              link: link,
+            });
+          }
+        }
+
+        videoData.sort((a, b) => b.views - a.views);
+        return videoData;
+      } catch (error) {
+        console.error("Error processing links", error);
+        throw new Error("Error processing links");
+      }
+    },
+  })
+);
